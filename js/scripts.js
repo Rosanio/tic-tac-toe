ba42e74c3591c7430b0a5568c56850303e8f523f
matt rosanio and tyler suderman <mmrosani@syr.edu>
2016-01-27 09:59:05 -0800

    add two space prototypes, a board constructor and a board prototype

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,31 @@ var Player = function(symbol) {
 var Space = function(xCoordinate, yCoordinate) {
   this.xCoordinate = xCoordinate;
   this.yCoordinate = yCoordinate;
+  this.empty = true;
+  this.symbol = "";
 }
+
+var Board = function() {}
 
 Player.prototype.mark = function() {
   return this.symbol;
+}
+
+Space.prototype.markedBy = function(player) {
+  return player;
+}
+
+Space.prototype.clicked = function() {
+  this.empty = false;
+  return this.empty;
+}
+
+Board.prototype.makeSpaces = function() {
+  var boardMatrix = [[],[],[]];
+  for(var i = 1; i <= 3; i++) {
+    for(var j = 1; j <= 3; j++) {
+      boardMatrix[i-1,j-1] = new Space(i,j);
+    }
+  }
+  return boardMatrix;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -15,4 +15,29 @@ describe('Space', function() {
     var testSpace = new Space(1,2);
     expect(testSpace.yCoordinate).to.equal(2);
   });
+
+  it("lets a player mark a space", function() {
+    var testPlayer = new Player("X")
+    var testSpace = new Space(1,2);
+    testSpace.markedBy(testPlayer)
+    expect(testSpace.markedBy(testPlayer)).to.equal(testPlayer);
+  });
+
+  it("will change the empty property of a Space when clicked is run", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.clicked()).to.equal(false);
+  })
+});
+
+describe('Board', function() {
+  it("creates 9 spaces when it is initialized", function() {
+    var testBoard = new Board();
+    var testBoardMatrix = [[],[],[]];
+    for(var i = 1; i <= 3; i++) {
+      for(var j = 1; j <= 3; j++) {
+        testBoardMatrix[i-1,j-1] = new Space(i,j);
+      }
+    }
+    expect(testBoard.makeSpaces()).to.eql(testBoardMatrix);
+  });
 });
